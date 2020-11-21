0c84e9a7ccdca98ae8bdefdfaba9f42c1bfa215e
Navaz Naz <muhammednavazmk@gmail.com>
2020-11-21 20:17:31 +0300

    second day

--- product/product.js
+++ product/product.js
@@ -1,12 +1,21 @@
-var db=require('../config/connection')
+var db = require("../config/connection");
+var collections=require('../config/collections')
 
-module.exports={
-    addProduct:(product,callback)=>{
+module.exports = {
+  addProduct: (product, callback) => {
     console.log(product);
-    
-    db.get().collection('product').insertOne(product).then((data) =>{
-        
-        callback(data.ops[0]._id)
-    })
-}
-}
+
+    db.get()
+      .collection("product")
+      .insertOne(product)
+      .then((data) => {
+        callback(data.ops[0]._id);
+      });
+  },
+  getAllproducts:()=>{
+      return new Promise(async(resolve,reject)=>{
+          let product=db.get().collection(collections.PRODUCT).find().toArray()
+          resolve(product)
+      })
+  }
+};
--- routes/admin.js
+++ routes/admin.js
@@ -4,37 +4,10 @@ var productH=require('../product/product')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  let products = [
-    {
-      name: "iphone",
-      category: "phone",
-      description: "this is demo",
-      Image:
-        "https://image-us.samsung.com/SamsungUS/home/mobile/phones/galaxy-s/galaxy-s20-fe-5g-images/cloud-navy/PDP-GALLERY-S20-FE-cloud-Navy-Lockup-01-1600x1200.jpg",
-    },
-    {
-      name: "max",
-      category: "food",
-      description: "this is demo",
-      Image:
-        "https://zdnet3.cbsistatic.com/hub/i/r/2020/10/12/ff9c5eaf-ac57-4f16-a790-88984ec29123/resize/1200xauto/9e19766233c6145e0f58e227c7c00dea/asus-rog-phone-3.jpg",
-    },
-    {
-      name: "iphone",
-      category: "phone",
-      description: "this is demo",
-      Image:
-        "https://image-us.samsung.com/SamsungUS/home/mobile/phones/galaxy-s/galaxy-s20-fe-5g-images/cloud-navy/PDP-GALLERY-S20-FE-cloud-Navy-Lockup-01-1600x1200.jpg",
-    },
-    {
-      name: "iphone",
-      category: "phone",
-      description: "this is demo",
-      Image:
-        "https://zdnet3.cbsistatic.com/hub/i/r/2020/10/12/ff9c5eaf-ac57-4f16-a790-88984ec29123/resize/1200xauto/9e19766233c6145e0f58e227c7c00dea/asus-rog-phone-3.jpg",
-    },
-  ];
-  res.render('admin/view-products',{admin:true,products});
+  productH.getAllproducts().then((products)=>{
+    res.render('admin/view-products',{admin:true,products});
+  })
+  
 });
 router.get('/add-product/',function(req,res){
   res.render('admin/add-product')
